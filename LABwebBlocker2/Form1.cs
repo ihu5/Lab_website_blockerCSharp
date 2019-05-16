2bca9983e92d488666497dc2b785125f7406c692
journey6 <blackflower24@gmail.com>
2019-05-16 04:42:30 +0300

    Add files via upload
    v1.0 with websites to be blocked are hardcoded

---
+++ LABwebBlocker2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LABwebBlocker2
+{
+        public partial class Form1 : Form
+    {
+        string entries = "127.0.0.1 youtube.com www.youtube.com" + "\n" +
+                            "127.0.0.1 twitch.tv www.twitch.com" + "\n" +
+                            "127.0.0.1 movs4u.tv www.movs4u.tv" + "\n" +
+                            "127.0.0.1 twitter.com www.twitter.com" + "\n" +
+                            "127.0.0.1 aflam.io www.aflam.io" + "\n" +
+                            "127.0.0.1 dailymotion.com www.dailymotion.com" + "\n" +
+                            "127.0.0.1 vimeo.com www.vimeo.com" + "\n" +
+                            "127.0.0.1 metacafe.com www.metacafe.com" + "\n" +
+                            "127.0.0.1 d.tube www.d.tube" + "\n" +
+                            "127.0.0.1 whatsapp.com www.whatsapp.com web.whatsapp.com" + "\n" +
+                            "127.0.0.1 facebook.com www.facebook.com" + "\n" +
+                            "127.0.0.1 instagram.com www.instagram.com" + "\n" +
+                            "127.0.0.1 ask.fm www.ask.fm" + "\n" +
+                            "127.0.0.1 myspace.com www.myspace.com" + "\n" +
+                            "127.0.0.1 veoh.com www.veoh.com" + "\n" +
+                            "127.0.0.1 fushaar.com www.fushaar.com" + "\n" +
+                            "127.0.0.1 te3b.com www.te3b.com" + "\n" +
+                            "127.0.0.1 9gag.com www.9gag.com";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+           
+            if (ModifyHostsFile(entries))
+            {
+                Console.WriteLine("Successfully blocked websites....");
+                label1.ForeColor = System.Drawing.Color.Green;
+                label1.Text = "Successfully blocked websites";
+                //Console.ReadLine();
+            }
+            else
+            {
+                label1.ForeColor = System.Drawing.Color.Red;
+                label1.Text = "Error! failed to block websites";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+
+        public static bool ModifyHostsFile(string entry)
+        {
+            try
+            {
+                String hostPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts");
+                System.IO.File.WriteAllText(hostPath, string.Empty);
+
+                using (System.IO.StreamWriter w = System.IO.File.AppendText(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts")))
+                {
+                    // clear hosts content
+                    
+                    
+                    w.WriteLine(entry);
+                    return true;
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool UnblockHostsFile()
+        {
+            try
+            {
+                String hostPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts");
+                System.IO.File.WriteAllText(hostPath, string.Empty);
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (UnblockHostsFile())
+            {
+                Console.WriteLine("Successfully unblocked websites....");
+                label1.ForeColor = System.Drawing.Color.Green;
+                label1.Text = "Successfully unblocked websites";
+                //Console.ReadLine();
+            }
+            else
+            {
+                label1.ForeColor = System.Drawing.Color.Red;
+                label1.Text = "Error! failed to unblock websites";
+            }
+
+        }
+    }
+}
+
+
---
+++ LABwebBlocker2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Diagnostics;
+
+namespace LABwebBlocker2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool administrativeMode = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!administrativeMode)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.Verb = "runas";
+                startInfo.FileName = Application.ExecutablePath;
+                try
+                {
+                    Process.Start(startInfo);
+                }
+                catch
+                {
+                    return;
+                }
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
